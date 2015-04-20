70dd007efb3d85d7a0c61acb870157400a8d88d5
vjdavid <vjdavid@gmail.com>
2015-04-20 12:15:41 -0700

    added controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,27 @@
 class ArticlesController < ApplicationController
-
-  def new
-  end
+  before_action :find_article, only: [:show, :edit]
 
   def create
-    render plain: params[:article].inspect
+   @article = Article.create(article_params)
+   @article.save
+
+   redirect_to @article
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  private
+  def article_params
+    params.permit(:name, :description)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 
 end
